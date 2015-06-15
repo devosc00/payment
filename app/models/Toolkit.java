25eb42cc4c0e75aaee67cf9c4ac65bb5c9a3f481
rafa <rafa@rafa-THINKPAD>
2015-06-15 07:10:00 +0200

    nie działa konwersja do json

---
+++ app/models/Toolkit.java
@@ -0,0 +1,15 @@
+package models;
+
+/**
+ * Created by rafa on 15.06.15.
+ */
+public class Toolkit {
+
+    public static class Material {
+        String name;
+        int unitPrice;
+        int quantity;
+
+    }
+
+}
